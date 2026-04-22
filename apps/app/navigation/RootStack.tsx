801e5bb855e9add82ab83de025e8fcfb5499015d
Jana <jana.pries@icloud.com>
2026-04-22 09:45:16 +0200

    bug fix
    navigaton

--- apps/app/navigation/RootStack.tsx
+++ apps/app/navigation/RootStack.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import ListView from '../src/presentation/PostListScreen'
-import CreatePost from '../src/presentation/PostFormScreen'
-import DetailScreen from '../src/presentation/PostDetailScreen'
+import PostListScreen from '../src/presentation/PostListScreen';
+import PostFormScreen from '../src/presentation/PostFormScreen';
+import PostDetailScreen from '../src/presentation/PostDetailScreen';
 
 export type RootStackParamList = {
   Home: undefined;
@@ -17,9 +17,9 @@ const Stack = createNativeStackNavigator();
 export function RootStack() {
   return (
     <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={ListView} />
-      <Stack.Screen name="Create" component={CreatePost} />
-      <Stack.Screen name="Detail" component={DetailScreen} />
+      <Stack.Screen name="Home" component={PostListScreen} />
+      <Stack.Screen name="Create" component={PostFormScreen} />
+      <Stack.Screen name="Detail" component={PostDetailScreen} />
     </Stack.Navigator>
   );
 }
